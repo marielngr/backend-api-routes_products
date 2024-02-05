7799f34061c06315155ca606bcc9e7b2ef969dc9
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-02-05 12:30:36 +0100

    endpoint for single product

---
+++ pages/api/products/[id].js
@@ -0,0 +1,10 @@
+import { getProductById } from "@/services/productServices.js";
+
+export default function handler(request, response) {
+  if (request.method === "GET") {
+    const { id } = request.query;
+    response.status(200).json(getProductById(id));
+    return;
+  }
+  response.status(405).json({ message: "Method not allowed" });
+}
