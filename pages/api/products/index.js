02d48beeeea23dc656d0579b5a6d3dee31490579
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-02-05 11:28:00 +0100

    api all products

---
+++ pages/api/products/index.js
@@ -0,0 +1,8 @@
+import { getAllProducts } from "@/services/productServices.js";
+
+export default function handler(request, response) {
+  if (request.method === "GET") {
+    response.status(200).json(getAllProducts());
+    return;
+  }
+}
